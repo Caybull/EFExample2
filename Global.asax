9de9e1a1be50a981e1f7231edf902bc51d7240b2
Caybull <87078180+Caybull@users.noreply.github.com>
2022-01-24 14:57:11 -0600

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RyanEFExample.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RyanEFExample.Startup))]
+namespace RyanEFExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
